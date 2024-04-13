ce4cad3aa4c190e39fc1b0cc8110164e8ec8be47
Danilo Malta <danilocmalta@gmail.com>
2024-04-14 10:33:44 +1200

    3 sections code and final solution

--- Message Generator script.js
+++ Message Generator script.js
@@ -5,23 +5,43 @@ greeting("Roberto");
 
 const returnFirstSection = () => {
     const firstSentenceSection = ["You slept tight last night", "You didn't sleep much", "You didn't sleep at all"];
-    const inspiringMessage = [];
+    const inspiringMessage1 = [];
     const firstSentenceRandom = firstSentenceSection[Math.floor(Math.random() * 3)];
-    inspiringMessage.push(firstSentenceRandom);
-    return inspiringMessage
+    inspiringMessage1.push(firstSentenceRandom);
+    return inspiringMessage1
 }
 
 
-console.log(returnFirstSection());
+// console.log(returnFirstSection());
+
+const returnSecondSection = () => {
+    const secondSentenceSection = ["and feel sleepy and tired.", "and feel incredibly brand new.", "and feel happy.", "and feel angry."];
+    const inspiringMessage2 = [];
+    const secondSentenceRandom = secondSentenceSection[Math.floor(Math.random() * 4)];
+    inspiringMessage2.push(secondSentenceRandom);
+    return inspiringMessage2
+}
+
+// console.log(returnSecondSection());
+
+const returnThirdSection = () => {
+    const thirdSentenceSection = ["Today holds great experiences for you.", "Today you will find kindness and affection all around you.", "Today you will accomplish what you have been dreaming of.", "Today will be another day to survive."];
+    const inspiringMessage3 = [];
+    const thirdSentenceRandom = thirdSentenceSection[Math.floor(Math.random() * 4)];
+    inspiringMessage3.push(thirdSentenceRandom);
+    return inspiringMessage3
+}
+
+// console.log(returnThirdSection());
 
 
-const secondSentenceSection = ["and feeel sleepy and tired.", "and feel incredibly brand new.", "and feel happy", "and feel angry"];
-const thirdSentenceSection = ["Today holds great experiences for you", "Today you will find kindness and affection all around you", "Today you will accomplish what you have been dreaming of", "Today will be another day to survive"]
+const returnAllSections = () => {
+return console.log(`${returnFirstSection()} ${returnSecondSection()} ${returnThirdSection()}`);
+};
 
-/* start creating arrays - perhaps one array for every group of possible sentence parts. 
-So, after randonly choosing each section of the 3 sentences, 
-we can nested loop among them and create a full sentence (using new array.push) as a result of the join( ) array function between them. 
-*/
+returnAllSections();
+
+
 
 
 
