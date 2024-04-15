60d5e7c6cf03dd991bb49dafcc3f492dc9c2aa72
Danilo Malta <danilocmalta@gmail.com>
2024-04-15 21:06:24 +1200

    final solution improved

--- Message Generator script.js
+++ Message Generator script.js
@@ -4,11 +4,9 @@ const greeting = (userName) => {
 greeting("Roberto");
 
 const returnFirstSection = () => {
-    const firstSentenceSection = ["You slept tight last night", "You didn't sleep much", "You didn't sleep at all"];
-    const inspiringMessage1 = [];
+    const firstSentenceSection = ["You slept tight last night", "You didn't sleep much", "You didn't sleep at all"];    
     const firstSentenceRandom = firstSentenceSection[Math.floor(Math.random() * 3)];
-    inspiringMessage1.push(firstSentenceRandom);
-    return inspiringMessage1
+    return firstSentenceRandom
 }
 
 
@@ -16,28 +14,27 @@ const returnFirstSection = () => {
 
 const returnSecondSection = () => {
     const secondSentenceSection = ["and feel sleepy and tired.", "and feel incredibly brand new.", "and feel happy.", "and feel angry."];
-    const inspiringMessage2 = [];
     const secondSentenceRandom = secondSentenceSection[Math.floor(Math.random() * 4)];
-    inspiringMessage2.push(secondSentenceRandom);
-    return inspiringMessage2
+    return secondSentenceRandom
 }
 
 // console.log(returnSecondSection());
 
 const returnThirdSection = () => {
     const thirdSentenceSection = ["Today holds great experiences for you.", "Today you will find kindness and affection all around you.", "Today you will accomplish what you have been dreaming of.", "Today will be another day to survive."];
-    const inspiringMessage3 = [];
     const thirdSentenceRandom = thirdSentenceSection[Math.floor(Math.random() * 4)];
-    inspiringMessage3.push(thirdSentenceRandom);
-    return inspiringMessage3
+    return thirdSentenceRandom
 }
 
 // console.log(returnThirdSection());
 
 
+
 const returnAllSections = () => {
-return console.log(`${returnFirstSection()} ${returnSecondSection()} ${returnThirdSection()}`);
-};
+const inspiringMessage = [];
+inspiringMessage.push(`${returnFirstSection()} ${returnSecondSection()} ${returnThirdSection()}`);
+return console.log(inspiringMessage.join(' '));
+}
 
 returnAllSections();
 
